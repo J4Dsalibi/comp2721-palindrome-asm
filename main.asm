c1a110e00b11c9326c17fc68638c157719d94281
J4Dsalibi <jadelsalibi@gmail.com>
2026-03-21 21:47:35 -0700

    added base input/output code

--- main.asm
+++ main.asm
@@ -10,30 +10,27 @@ section .text
 
 _start:
 
-    ; ---- print prompt ----
-    mov eax, 4          ; syscall: write
-    mov ebx, 1          ; stdout
-    mov ecx, prompt
-    mov edx, prompt_len
-    int 0x80
+    mov rax, 1 ; syscall: write
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, prompt_len
+    syscall
 
-    ; ---- read input ----
-    mov eax, 3          ; syscall: read
-    mov ebx, 0          ; stdin
-    mov ecx, input
-    mov edx, 100
-    int 0x80
+    mov rax, 0 ; syscall: read
+    mov rdi, 0
+    mov rsi, input
+    mov rdx, 100
+    syscall
 
-    mov esi, eax        ; store length
+    mov rbx, rax ; store length
 
-    ; ---- print input ----
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, input
-    mov edx, esi
-    int 0x80
+    ; print input
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, input
+    mov rdx, rbx ; print only actual length
+    syscall
 
-    ; ---- exit ----
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    mov rax, 60 ; syscall: exit
+    xor rdi, rdi
+    syscall
